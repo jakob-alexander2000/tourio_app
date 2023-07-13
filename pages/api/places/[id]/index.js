31fd35a5e8f5c994e15ebfc9250a7e619fdf4a3f
Saba <sabarht@gmail.com>
2023-07-13 15:43:32 +0200

    reading from database

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,4 +1,4 @@
-import { places } from '../../../../lib/db.js';
+import { places } from "../../../../lib/db.js";
 
 export default function handler(request, response) {
   const { id } = request.query;
@@ -7,10 +7,10 @@ export default function handler(request, response) {
     return;
   }
 
-  const place = places.find((place) => place.id === id);
+  const place = places.find((place) => place._id === id);
 
   if (!place) {
-    return response.status(404).json({ status: 'Not found' });
+    return response.status(404).json({ status: "Not found" });
   }
 
   response.status(200).json(place);
